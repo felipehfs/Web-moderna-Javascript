fc6fcffbc4653396ec6df154c66779c9042cd6fa
Felipe Henrique <felipehfsouza@gmail.com>
2018-09-24 19:47:47 -0300

    Adicionado vue calculator

---
+++ vue/calculator/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+        <h1>Calculadora</h1>
+        <Calculator />
+    </div>
+</template>
+
+<script>
+import Calculator from './main/Calculator'
+
+export default {
+    components: { Calculator}
+}
+</script>
+
+<style>
+*{
+    font-family: monospace;
+}
+
+body {
+    margin: 0;
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    color: #fff;
+    background: linear-gradient(to right, rgba(147,206,222,1) 0%, rgba(117,189,209,1) 41%, rgba(73,165,191,1) 100%);
+    justify-content: center;
+    align-items: center;
+}
+</style>
